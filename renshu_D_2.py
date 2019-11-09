1fecd3a69093fbad004c1f0a5035bee6e439e4c0
tatsuya_ego <ts2000kasa@gmail.com>
2019-11-10 01:22:03 +0900

    課題D終了

---
+++ renshu_D_2.py
@@ -0,0 +1,18 @@
+bingo = [
+    [0, 1, 1, 1, 1],
+    [0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1],
+    [1, 1, 0, 1, 0],
+]
+
+hantei = 0
+
+for m in range(5):
+    for n in range(5):
+        if bingo[m][m] == bingo[n][m]:
+            hantei += 1
+    if hantei == 5 or hantei == 0:
+        print(str(m))
+    hantei = 0
+
