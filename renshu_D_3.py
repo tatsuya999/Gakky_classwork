66b69a94d3299f1d3422ba5c07023837b8eb8a99
tatsuya_ego <ts2000kasa@gmail.com>
2019-11-10 02:51:40 +0900

    fix

--- renshu_D_3.py
+++ renshu_D_3.py
@@ -12,7 +12,7 @@ for m in range(5):
     for n in range(5):
         if bingo[m][m] == bingo[m][n]:
             hantei += 1
-    if hantei == 5 or hantei == 0:
+    if hantei == 5:
         bingocount += 1
     hantei = 0
 # 縦判定
@@ -20,7 +20,7 @@ for m in range(5):
     for n in range(5):
         if bingo[m][m] == bingo[n][m]:
             hantei += 1
-    if hantei == 5 or hantei == 0:
+    if hantei == 5:
         bingocount += 1
     hantei = 0
 
@@ -28,14 +28,14 @@ for m in range(5):
 for n in range(5):
     if bingo[0][0] == bingo[n][n]:
         hantei += 1
-if hantei == 5 or hantei == 0:
+if hantei == 5:
     bingocount += 1
 hantei = 0
 # 斜め判定/
 for n in range(5):
     if bingo[0][4] == bingo[n][4 - n]:
         hantei += 1
-if hantei == 5 or hantei == 0:
+if hantei == 5:
     bingocount += 1
 hantei = 0
 
